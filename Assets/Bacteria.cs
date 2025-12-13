af74b800dc60c32cee61474be4a3ff85ae0872d4
DtheMakeeeR <danilgol45@mail.ru>
2025-12-13 15:29:35 +0300

    start

---
+++ Assets/Bacteria.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Bacteria : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Vector2 direction;
+    [SerializeField]
+    private Color color;
+    private SpriteRenderer spriteRenderer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 vector = direction.normalized * speed * Time.deltaTime;
+        transform.position = transform.position + new Vector3(vector.x, vector.y, 0);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("TriggerExit");
+        if (collision.gameObject.tag == "World")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/World.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Vector2 direction;
+    [SerializeField]
+    private float coolDown;
+    public float BornTime;
+    public float BacteriaCoefficent;
+    public float WorldCoefficent;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Coroutine coroutine;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 vector = direction.normalized * speed * Time.deltaTime;
+        transform.position = transform.position + new Vector3(vector.x, vector.y, 0);
+        if(coroutine == null) coroutine = StartCoroutine(Change());
+    }
+
+    IEnumerator Change()
+    {
+        Debug.Log("Change");
+        yield return new WaitForSeconds(coolDown);
+        int speedSign = Random.value >= 0.5 ? 1 : -1;
+        int rotSign = Random.value >= 0.5 ? 1 : -1;
+        Debug.Log($"speedSign: {speedSign}");
+        Debug.Log($"rotSign: {rotSign}");
+        speed = speed + speed*WorldCoefficent*speedSign;
+        direction = Random.onUnitSphere;
+        coroutine = null;
+    }
+}
